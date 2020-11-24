2011410df0bc13e8bd1a2edc786ccd585fdbba4b
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2020-11-24 18:56:28 +0300

    complete 1 - 4 test

--- src/memoize.js
+++ src/memoize.js
@@ -1,3 +1,14 @@
-export function memoize() {
-    // TODO
+export function memoize(inFunc) {
+    if (typeof inFunc !== 'function') {
+        return null;
+    }
+    const cache = new Map();
+    return function outFunc(x) {
+        if (cache.has(x)) {
+            return cache.get(x);
+        }
+        const result = inFunc.call(this, x);
+        cache.set(x, result);
+        return result;
+    };
 }
